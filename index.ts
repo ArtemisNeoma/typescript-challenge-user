1f2b482c84f89f31a9f91c32e7df2881430541b2
Neo-WRN <nicolas.destito@warren.com.br>
2022-04-28 17:17:10 -0300

    export default app added to app.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import helmet from 'helmet'
 //import routes from '/path/to/Routes.ts"
 
 const app: Express = express()
-//app.use(helmet())
+app.use(helmet())
 app.use(json())
 //app.use(routes)
+
+export default app
