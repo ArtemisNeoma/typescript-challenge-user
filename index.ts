6cb3d605c66f59eb4e744afc1e0bfdf624af7724
Neo-WRN <nicolas.destito@warren.com.br>
2022-04-28 16:15:32 -0300

    initial configuration with tsnd compiling

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import { Express, json } from 'express'
+import helmet from 'helmet'
+//import routes from '/path/to/Routes.ts"
+
+const app: Express = express()
+//app.use(helmet())
+app.use(json())
+//app.use(routes)
