691b4d2fbae3b9a704ee5c48c970152952374b73
Neo-WRN <nicolas.destito@warren.com.br>
2022-04-28 18:03:25 -0300

    bin/www.ts basic configuration done

---
+++ bin/www.ts
@@ -0,0 +1,37 @@
+import app from '../index'
+import { config } from 'dotenv'
+import { createServer } from 'http'
+
+config( {path: './config/config.env'} )
+const port = process.env.PORT || 3000
+app.set("port", port)
+const server = createServer(app)
+server.listen(port)
+
+server.on("error", onError)
+server.on("listening", onListening)
+
+function pipeOrPort(address: string | any) :string {
+    return typeof address == "string" ? `pipe ${address}` : `port ${address}`
+}
+
+function onError(error: NodeJS.ErrnoException) :void {
+    if (error.syscall != "listen") throw error
+
+    let bind = pipeOrPort(server.address)
+    switch (error.code) {
+        case "EACESS":
+            console.log(`${bind} requires elevated privileges`)
+            process.exit(1)
+        case "EADDRINUSE":
+            console.log(`${bind} is already in use`)
+            process.exit(1)
+        default:
+            throw error
+    }
+}
+
+function onListening() {
+    let bind = pipeOrPort(server.address)
+    console.log(`Listening on ${bind}`)
+}
