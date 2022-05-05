d4dfb192dd5fbc42fe919b394004028118820deb
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-05 16:03:50 -0300

    feat: created new route /user at customer.ts

--- presentation/routes/customer.ts
+++ presentation/routes/customer.ts
@@ -3,6 +3,7 @@ import createCustomer from '../controllers/UserController'
 import customerValidators from '../validators/customer-validators'
 
 const router = Router()
-router.post('/', customerValidators, createCustomer) //, customerValidators, createCustomer)
+router.post('/', customerValidators, createCustomer)
+router.get('/user')
 
 export default router
