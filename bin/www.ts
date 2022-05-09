3af79696cb1d34de51601c74a1e52abc47b61ad7
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-09 13:12:56 -0300

    feat: moved onListening, onError and pipeOrPort from www.ts to serverInfo.ts

--- bin/www.ts
+++ bin/www.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { config } from 'dotenv';
 import { createServer } from 'http';
+import { onListening, onError } from '../util/serverInfo';
 import app from '../index';
 
 config({ path: './config/config.env' });
@@ -8,34 +9,7 @@ const port = process.env.PORT || 3000;
 app.set('port', port);
 const server = createServer(app);
 
-function pipeOrPort(address: string | any) :string {
-  return typeof address === 'string' ? `pipe ${address}` : `port ${address.port}`;
-}
-
-function onError(error: NodeJS.ErrnoException) :never {
-  if (error.syscall !== 'listen') throw error;
-
-  const bind = pipeOrPort(server.address);
-  switch (error.code) {
-    case 'EACESS':
-      console.log(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.log(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  const bind = pipeOrPort(server.address());
-  console.log(`Listening on ${bind}`);
-}
-
 server.listen(port);
-
-server.on('error', onError);
-server.on('listening', onListening);
+console.log(server.address());
+server.on('error', (error) => onError(error, server));
+server.on('listening', () => onListening(server));
---
+++ util/serverInfo.ts
@@ -0,0 +1,29 @@
+import { Server } from 'http';
+
+/* eslint-disable no-console */
+export function pipeOrPort(address: string | any) :string {
+  return typeof address === 'string' ? `pipe ${address}` : `port ${address.port}`;
+}
+
+export function onError(error: NodeJS.ErrnoException, server: Server) :never {
+  if (error.syscall !== 'listen') throw error;
+
+  const bind = pipeOrPort(server.address);
+  switch (error.code) {
+    case 'EACESS':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+export function onListening(server: Server) {
+  const bind = pipeOrPort(server.address());
+  console.log(`Listening on ${bind}`);
+}
