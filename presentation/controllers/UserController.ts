eca134d9450ffdc5ba039dfb689d4b7cb7fb725a
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-09 10:20:34 -0300

    bug: fixed style at UserControllers, functions create/getCustomer are now static

--- presentation/controllers/UserController.ts
+++ presentation/controllers/UserController.ts
@@ -1,21 +1,20 @@
-import { Request, Response, NextFunction } from "express";
-import { validationResult } from "express-validator";
-import Users from '../../domain/user/mocks/UserMock'
+import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import Users from '../../domain/user/mocks/UserMock';
 
 class UserController {
-
-    createCustomer(req: Request, res: Response, next: NextFunction) {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(422).json( { errors: errors.mapped() } )
-        }
-        Users.push(req.body)
-        res.sendStatus(200)
+  static createCustomer(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
     }
+    Users.push(req.body);
+    return res.sendStatus(200);
+  }
 
-    getCustomers(req: Request, res: Response, next: NextFunction) {
-        res.status(200).send(Users)
-    }
+  static getCustomers(req: Request, res: Response) {
+    res.status(200).send(Users);
+  }
 }
 
-export default new UserController
+export default UserController;
