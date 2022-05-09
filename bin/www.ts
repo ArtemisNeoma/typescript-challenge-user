9dc5cbd3ab6535f11a468432c97f232d8fe5122c
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-09 13:15:45 -0300

    feat: removed console.log from www.ts

--- bin/www.ts
+++ bin/www.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { config } from 'dotenv';
 import { createServer } from 'http';
 import { onListening, onError } from '../util/serverInfo';
@@ -10,6 +9,5 @@ app.set('port', port);
 const server = createServer(app);
 
 server.listen(port);
-console.log(server.address());
 server.on('error', (error) => onError(error, server));
 server.on('listening', () => onListening(server));
