d15c99dfd69b490c136d96484b22f292a79304e0
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-09 10:48:57 -0300

    bug: fixed style at www.ts

--- bin/www.ts
+++ bin/www.ts
@@ -1,37 +1,41 @@
-import app from '../index'
-import { config } from 'dotenv'
-import { createServer } from 'http'
+/* eslint-disable no-console */
+import { config } from 'dotenv';
+import { createServer } from 'http';
+import app from '../index';
 
-config( {path: './config/config.env'} )
-const port = process.env.PORT || 3000
-app.set("port", port)
-const server = createServer(app)
-server.listen(port)
-
-server.on("error", onError)
-server.on("listening", onListening)
+config({ path: './config/config.env' });
+const port = process.env.PORT || 3000;
+app.set('port', port);
+const server = createServer(app);
 
 function pipeOrPort(address: string | any) :string {
-    return typeof address == "string" ? `pipe ${address}` : `port ${address.port}`
+  return typeof address === 'string' ? `pipe ${address}` : `port ${address.port}`;
 }
 
-function onError(error: NodeJS.ErrnoException) :void {
-    if (error.syscall != "listen") throw error
+function onError(error: NodeJS.ErrnoException) :never {
+  if (error.syscall !== 'listen') throw error;
 
-    let bind = pipeOrPort(server.address)
-    switch (error.code) {
-        case "EACESS":
-            console.log(`${bind} requires elevated privileges`)
-            process.exit(1)
-        case "EADDRINUSE":
-            console.log(`${bind} is already in use`)
-            process.exit(1)
-        default:
-            throw error
-    }
+  const bind = pipeOrPort(server.address);
+  switch (error.code) {
+    case 'EACESS':
+      console.log(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 function onListening() {
-    let bind = pipeOrPort(server.address())
-    console.log(`Listening on ${bind}`)
+  const bind = pipeOrPort(server.address());
+  console.log(`Listening on ${bind}`);
 }
+
+server.listen(port);
+
+server.on('error', onError);
+server.on('listening', onListening);
