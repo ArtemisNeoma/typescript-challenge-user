c36cb4eba3a8156aed6cb314c926b3d79888f4c6
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-10 13:59:39 -0300

    feat: replaced console.log with winston logger, created logger serverLogger

--- bin/serverInfo.ts
+++ bin/serverInfo.ts
@@ -1,13 +1,40 @@
-/* eslint-disable no-console */
+import { createLogger, format, transports } from 'winston';
+
+const myFormat = format.printf(({
+  level, message, label, timestamp,
+}) => `[${timestamp} - ${label}] ${level}: ${message}`);
+
+const serverLogger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.label({ label: 'test' }),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    myFormat,
+  ),
+  defaultMeta: { service: 'Server Info' },
+  transports: [
+    new transports.Console(),
+  ],
+});
+
 export function onError(error: NodeJS.ErrnoException, port: string | number) :never {
   if (error.syscall !== 'listen') throw error;
   switch (error.code) {
     case 'EACESS':
-      console.error(`${port} requires elevated privileges`);
+      serverLogger.log({
+        level: 'error',
+        message: `${port} requires elevated privileges`,
+      });
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.log(`${port} is already in use`);
+      serverLogger.log({
+        level: 'error',
+        message: `Address ${port} is already in use`,
+      });
       process.exit(1);
       break;
     default:
@@ -16,5 +43,8 @@ export function onError(error: NodeJS.ErrnoException, port: string | number) :ne
 }
 
 export function onListening(port: string | number) {
-  console.log(`Listening on ${port}`);
+  serverLogger.log({
+    level: 'info',
+    message: `Listening on port ${port}`,
+  });
 }
