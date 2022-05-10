5f688d71039035e9ce348ba338d2fa4f1bcd0e25
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-10 10:17:43 -0300

    feat: removed both Server http and pipeOrPort from serverInfo.ts, onError and onListening now take the port directly as a parameter and not the server

--- util/serverInfo.ts
+++ util/serverInfo.ts
@@ -1,24 +1,13 @@
-import { Server } from 'http';
-
 /* eslint-disable no-console */
-export function pipeOrPort(address: string | any) :string {
-  return typeof address === 'string' ? `pipe ${address}` : `port ${address.port}`;
-}
-
-export function onError(error: NodeJS.ErrnoException, server: Server) :never {
+export function onError(error: NodeJS.ErrnoException, port: string | number) :never {
   if (error.syscall !== 'listen') throw error;
-  if (server.address() === null) {
-    console.log('Another instance of the server is already running.');
-    process.exit(1);
-  }
-  const bind = pipeOrPort(server.address);
   switch (error.code) {
     case 'EACESS':
-      console.error(`${bind} requires elevated privileges`);
+      console.error(`${port} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.log(`${bind} is already in use`);
+      console.log(`${port} is already in use`);
       process.exit(1);
       break;
     default:
@@ -26,7 +15,6 @@ export function onError(error: NodeJS.ErrnoException, server: Server) :never {
   }
 }
 
-export function onListening(server: Server) {
-  const bind = pipeOrPort(server.address());
-  console.log(`Listening on ${bind}`);
+export function onListening(port: string | number) {
+  console.log(`Listening on ${port}`);
 }
