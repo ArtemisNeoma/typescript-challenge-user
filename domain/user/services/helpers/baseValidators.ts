a6972fb3427f72f9527954d73ef18c2e274254e4
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-13 16:14:36 -0300

    feat: stringValidation changed to a arrow function

--- domain/user/services/helpers/baseValidators.ts
+++ domain/user/services/helpers/baseValidators.ts
@@ -6,6 +6,6 @@ const escapeHtmlString = (value: string, helpers: Joi.CustomHelpers) => {
   if (typeof value !== 'string') helpers.error('any.invalid');
   return sanitizeHtml(value);
 };
-export const stringValidation = string
+export const stringValidation = () => string
   .trim()
   .custom(escapeHtmlString, 'Sanitizes HTML code included in the string for safety');
--- domain/user/services/helpers/userValidators.ts
+++ domain/user/services/helpers/userValidators.ts
@@ -4,13 +4,13 @@ import { stringValidation } from './baseValidators';
 const { object } = Joi.types();
 
 const userSchema = object.keys({
-  full_name: stringValidation
+  full_name: stringValidation()
     .min(1)
     .message('Full name lenght must be at least 1')
     .max(256)
     .message('Full name can\'t be longer than 256 characters')
     .required(),
-  email: stringValidation
+  email: stringValidation()
     .email({ tlds: false })
     .required(),
 });
