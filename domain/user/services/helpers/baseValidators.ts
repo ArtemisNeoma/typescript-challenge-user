938672a5eec34aeb28411dc49209312e3acdd9c8
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-13 15:49:44 -0300

    feat: added new escapeHtmlString function as custom validation to stringValidation

--- domain/user/services/helpers/baseValidators.ts
+++ domain/user/services/helpers/baseValidators.ts
@@ -2,6 +2,10 @@ import Joi from 'joi';
 import sanitizeHtml from 'sanitize-html';
 
 const { string, number } = Joi.types();
+const escapeHtmlString = (value: string, helpers: Joi.CustomHelpers) => {
+  if (typeof value !== 'string') helpers.error('any.invalid');
+  return sanitizeHtml(value);
 };
 export const stringValidation = string
   .trim()
+  .custom(escapeHtmlString, 'Sanitizes HTML code included in the string for safety');
