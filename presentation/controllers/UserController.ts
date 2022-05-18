8806d888fceec012574f064402eb331b93906767
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-18 16:38:44 -0300

    refactor: added temporary console.log(Users) to UserController

--- presentation/controllers/UserController.ts
+++ presentation/controllers/UserController.ts
@@ -6,6 +6,7 @@ class UserController {
   static async createCustomer(req: Request, res: Response) {
     const userService = new UserService(Users);
     const serviceResponse = await userService.create(req.body);
+    console.log(Users);
     return res.status(serviceResponse.code).json(serviceResponse.error);
   }
 
