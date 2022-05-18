d6da8710214d2b8e3357a95bcd77ab2062bb7c42
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-18 19:21:34 -0300

    refactor: changed Joi.expression to simpler Joi.ref at schemaContext

--- util/services/schemaContext.ts
+++ util/services/schemaContext.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const getFromContext = (ref: string, property?: string) => {
-  const refPath = `{{$${ref}`;
-  return property === undefined ? `${Joi.expression(refPath)}}}` : Joi.expression(`${refPath}.${property}}}`);
+  const refPath = `$${ref}`;
+  return property === undefined ? Joi.ref(refPath) : Joi.ref(`${refPath}.${property}`);
 };
