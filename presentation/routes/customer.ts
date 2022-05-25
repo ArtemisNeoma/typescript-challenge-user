6707396cdacbd79e5634a72fad8144040dc66c31
Neo-WRN <nicolas.destito@warren.com.br>
2022-05-25 16:03:53 -0300

    feat: Created userValidationMiddleware and added to customer post route

--- presentation/routes/customer.ts
+++ presentation/routes/customer.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import userValidationMiddleware from '@middleware/User/userValidationMiddleware';
 import UserController from '../controllers/UserController';
 
 const router = Router();
-router.post('/', UserController.createCustomer);
+router.post('/', userValidationMiddleware, UserController.createCustomer);
 router.get('/', UserController.getCustomers);
 
 export default router;
